ea23cecad3fb7fb92ee1ccf1b2c0a38c7cef11f5
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-06-10 16:01:32 +0800

    remove readme and add r.sh

--- README.md
+++
@@ -1,8 +0,0 @@
-npx @vscode/vsce package --skip-license
-
-
-code --user-data-dir /tmp/foo/u --extensions-dir /tmp/foo/e --install-extension helloworld-minimal-sample-0.0.1.vsix
-
-code --user-data-dir /tmp/foo/u --extensions-dir /tmp/foo/e
-
-run command "Developer: Toggle Developer Tools" for viewing console.log messages
---
+++ r.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+npx @vscode/vsce package --skip-license
+
+THE_DIR=/tmp/foo
+
+rm -fR ${THE_DIR}
+
+code --user-data-dir ${THE_DIR}/u --extensions-dir ${THE_DIR}/e --install-extension helloworld-minimal-sample-0.0.1.vsix
+
+code --user-data-dir ${THE_DIR}/u --extensions-dir ${THE_DIR}/e --install-extension ms-vscode-remote.vscode-remote-extensionpack
+
+mkdir -p ${THE_DIR}/u/User
+echo '{"workbench.colorTheme": "Default Light+"}' > ${THE_DIR}/u/User/settings.json
+
+
+mkdir -p ${THE_DIR}/p/.devcontainer
+cat <<END > ${THE_DIR}/p/.devcontainer/devcontainer.json
+{
+  "name": "Minimal Dev Container",
+  "image": "mcr.microsoft.com/devcontainers/base:ubuntu",
+  "runArgs": ["--rm"]
+}
+END
+
+code --user-data-dir ${THE_DIR}/u --extensions-dir ${THE_DIR}/e  ${THE_DIR}/p
+
+# run command "Developer: Toggle Developer Tools" for viewing console.log messages
